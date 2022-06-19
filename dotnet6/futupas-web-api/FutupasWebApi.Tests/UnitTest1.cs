e8fa07fb54d0ebee027e616a5b9244ec50e85c54
Oleksandr Panov <oleksandr.panov@styrax.cz>
2022-06-19 18:16:51 +0200

    add: web api app sample

---
+++ dotnet6/futupas-web-api/FutupasWebApi.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using FutupasWebApi;
+
+namespace FutupasWebApi.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
